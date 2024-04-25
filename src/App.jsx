9b8e6eb4781744c17ac63790314eb7e2abfa93a1
Lucas Macias <lucasjmacias@gmail.com>
2024-04-24 21:38:16 -0300

    Definicion de components

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,35 @@
 import React, { useState } from "react";
-import { Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes, Link } from "react-router-dom";
 import Header from "./components/global/Header";
 
-export const App =() => {
+export const App = () => {
     return (
         <>
-            <Header />
+            <BrowserRouter>
+                <Header />
 
-            <Routes>
-                <Route path="/" element={<Inicio />}></Route>
-                <Route path="/perros" element={<Perros />}></Route>
-                <Route path="/gatos" element={<Gatos />}></Route>
-                <Route path="/contacto" element={<Contacto />}></Route>
-                <Route path="/error404" element={<Error404 />}></Route>
-                <Route path="/*" element={<Navigate to="/error404" />}></Route>
-            </Routes>
+                <Routes>
+                    <Route path="/" element={<Inicio />}></Route>
+                    <Route path="/perros" element={<Perros />}></Route>
+                    <Route path="/gatos" element={<Gatos />}></Route>
+                    <Route path="/contacto" element={<Contacto />}></Route>
+                    <Route
+                        path="/perros/detalle"
+                        element={<DetallePerro />}
+                    ></Route>
+                    <Route
+                        path="/gatos/detalle"
+                        element={<DetalleGato />}
+                    ></Route>
+                    <Route path="/error404" element={<Error404 />}></Route>
+                    <Route
+                        path="/*"
+                        element={<Navigate to="/error404" />}
+                    ></Route>
+                </Routes>
 
-            <Footer />
+                <Footer />
+            </BrowserRouter>
         </>
     );
-}
-
-
+};
