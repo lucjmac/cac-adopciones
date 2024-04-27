dc1a0627d3d8e57312c30e7e4fe790b06e78b99a
“Belu <belen.dominguez@mediamonks.com>
2024-04-26 21:57:59 -0300

    added new variables, added icons, modified heading

--- src/Components/Global/Header.jsx
+++ src/Components/Global/Header.jsx
@@ -3,65 +3,57 @@ import { NavLink, Link } from "react-router-dom";
 // import logo from "../../assets/img/";
 
 const Header = () => {
-    return (
-        <header id="header" className="fixed-top d-flex align-items-center">
-            <div className="container d-flex align-items-center">
+  return (
+    <header id="header" className="fixed-top d-flex align-items-center">
+      <div className="container d-flex align-items-center">
+        <NavLink to="/" className="logo me-auto">
+          <img src={""} alt="logo" />
+        </NavLink>
 
-                <NavLink to="/" className="logo me-auto">
-                    <img src={""} alt="logo" />
-                </NavLink>
-
-                <nav id="navbar" className="navbar order-last order-lg-0">
-                    <ul>
-                        <li>
-                            <NavLink to="/" className="nav-link scrollto active">
-                                Inicio
-                            </NavLink>
-                        </li>
-                        <li>
-                            <Link to = "#acerca-de" className="nav-link scrollto">
-                                Acerca de Nosotros
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to = "#requisitos" className="nav-link scrollto">
-                                Requisitos para adoptar
-                            </Link>
-                        </li>
-                        <li className="dropdown">
-                            <a className="nav-link scrollto">
-                                <span>Animales en adopcion</span>
-                                <i className="bi bi-chevron-down"></i>
-                            </a>
-                            <ul>
-                                <li>
-                                    <NavLink
-                                        to="/perros"
-                                        className="nav-link scrollto"
-                                    >
-                                        Perros en adopcion
-                                    </NavLink>
-                                </li>
-                                <li>
-                                    <NavLink
-                                        to="/gatos"
-                                        className="nav-link scrollto"
-                                    >
-                                        Gatos en adopcion
-                                    </NavLink>
-                                </li>
-                            </ul>
-                        </li>
-                        <li>
-                            <NavLink to="/contacto">Contacto</NavLink>
-                        </li>
-                    </ul>
-                    <i className="bi bi-list mobile-nav-toggle"></i>
-                </nav>
-            </div>
-        </header>
-    );
+        <nav id="navbar" className="navbar order-last order-lg-0">
+          <ul>
+            <li>
+              <NavLink to="/" className="nav-link scrollto active">
+                Inicio
+              </NavLink>
+            </li>
+            <li>
+              <a href="#acerca-de" className="nav-link scrollto">
+                Acerca de Nosotros
+              </a>
+            </li>
+            <li>
+              <Link to="#requisitos" className="nav-link scrollto">
+                Requisitos para adoptar
+              </Link>
+            </li>
+            <li className="dropdown">
+              <a className="nav-link scrollto">
+                <span>Animales en adopcion</span>
+                <i className="bi bi-chevron-down"></i>
+              </a>
+              <ul>
+                <li>
+                  <NavLink to="/perros" className="nav-link scrollto">
+                    Perros en adopcion
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/gatos" className="nav-link scrollto">
+                    Gatos en adopcion
+                  </NavLink>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <NavLink to="/contacto">Contacto</NavLink>
+            </li>
+          </ul>
+          <i className="bi bi-list mobile-nav-toggle"></i>
+        </nav>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
-
--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 import Nosotros from "../Components/Content/Nosotros";
-import Requisitos from "../Components/Content/Requisitos";
+import Requisitos from "../Components/Content/Requisitos/Requisitos";
 
 const Inicio = () => {
   return (
