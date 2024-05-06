0cba28648ecb47d09fcb56fdccade7ebb4190c23
“Belu <belen.dominguez@mediamonks.com>
2024-05-06 19:56:54 -0300

    created new footer

--- src/Components/Global/Footer/Footer.jsx
+++ src/Components/Global/Footer/Footer.jsx
@@ -1,63 +1,69 @@
-import React, { useState } from "react";
-import { NavLink } from "react-router-dom";
-import Heading from "../../Atoms/Heading/Heading";
-import { FaInstagram, FaTwitter, FaFacebookF } from "react-icons/fa";
+import { CiSearch } from "react-icons/ci";
+import { FaFacebookF, FaInstagram, FaTwitter, FaTiktok } from "react-icons/fa";
 
 import "./Footer.css";
 
 const Footer = () => {
   return (
     <footer>
-      <div className="detail-wrapper">
-        <div className="details">
-          <h3 className="logo">
-            Adopteros<span>.</span>{" "}
-          </h3>
-          <p>
-            <strong>Telefono:</strong> 11 1111 1111
-          </p>
-          <p>
-            <strong>Email:</strong> adopteros@adopteros.com
-          </p>
+      <div className="footer-col">
+        <div className="footer-search">
+          <div className="icon">
+            <CiSearch />
+          </div>
+          <input type="text" placeholder="Buscador" />
         </div>
-        <div className="navigation">
-          <Heading as="h4">Enlaces de utilidad</Heading>
-          <nav className="nav-list">
-            <ul>
-              <li>
-                <NavLink to="/" className="nav-item">
-                  <span>></span> Inicio
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to="/animales" className="nav-item">
-                  <span>></span> Animales en adopción
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to="/contacto" className="nav-item">
-                  <span>></span> Contacto
-                </NavLink>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </div>
-      <div className="copyright">
-        <p>© Copyright Adopteros Argentina. 2022 All Rights Reserved</p>
-        <div className="social">
-          <ul className="social-list">
+        <div className="footer-social-nav footer-anchors">
+          <ul>
             <li>
-              <FaInstagram />
+              <a href="/">
+                <FaFacebookF />
+              </a>
             </li>
             <li>
-              <FaTwitter />
+              <a href="/">
+                <FaInstagram />
+              </a>
             </li>
             <li>
-              <FaFacebookF />
+              <a href="/">
+                <FaTwitter />
+              </a>
+            </li>
+            <li>
+              <a href="/">
+                <FaTiktok />
+              </a>
             </li>
           </ul>
         </div>
+      </div>
+      <div className="footer-col footer-col-nav">
+        <nav className="footer-menus footer-anchors  footer-internal-nav">
+          <h3>elGourmet</h3>
+          <ul>
+            <li>
+              <a href="/">Chefs</a>
+            </li>
+            <li>
+              <a href="/">Glosario</a>
+            </li>
+            <li>
+              <a href="/">Novedades</a>
+            </li>
+          </ul>
+        </nav>
+        <nav className="footer-menus footer-anchors footer-company-nav">
+          <h3>Compañia</h3>
+          <ul>
+            <li>
+              <a href="/">Politicas de privacidad</a>
+            </li>
+            <li>
+              <a href="/">Términos y condiciones</a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </footer>
   );
