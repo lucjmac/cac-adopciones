758240c84db66c11fb6dffb962d9a02e05d671d3
Lucas Macias <lucasjmacias@gmail.com>
2024-05-06 20:24:10 -0300

    Search pasa al Header

--- src/Components/Global/Header.jsx
+++ src/Components/Global/Header.jsx
@@ -2,12 +2,13 @@ import React, { useState } from "react";
 import { NavLink, Link } from "react-router-dom";
 // import logo from "../../assets/img/";
 
-import Recetas from "../../pages/Recetas";
+import Search from "../Global/Search";
 
 const Header = () => {
     return (
         <>
             <h1>Header</h1>
+            <Search />
         </>
     );
 };
