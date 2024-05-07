b885367e01199b203042f095e26ebaf0bb7d9693
“Belu <belen.dominguez@mediamonks.com>
2024-05-07 20:51:52 -0300

    fix inicio import issue

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import RecetasGrid from "../Components/Content/RecetasGrid";
 
 /*import Nosotros from "../components/content/Nosotros";
 import Requisitos from "../components/content/Requisitos";*/
