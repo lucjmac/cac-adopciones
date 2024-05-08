9402bc2048f7fc77e2cf6341aea2d1f6eba797b5
Angela Delfino <angelairisguevara@gmail.com>
2024-05-08 05:46:44 -0300

    Modifica estilos y agrega valor por defecto al input si existe en los query params

--- src/Components/Search/Search.jsx
+++ src/Components/Search/Search.jsx
@@ -1,15 +1,24 @@
 import React, { useState, useEffect } from "react";
-import { NavLink, Navigate } from "react-router-dom";
+import { Navigate, useSearchParams } from "react-router-dom";
+import { CiSearch } from "react-icons/ci";
+import styles from "./Search.module.css";
 
-const Search = () => {
+const Search = ({ reversed }) => {
   const [searchParam, setSearchParam] = useState("");
+  const [searchParams] = useSearchParams();
+  const defaultSearch = searchParams.get("search");
 
   const handleSubmit = (event) => {
     event.preventDefault();
     const elements = new FormData(event.currentTarget);
     const search = elements.get("search");
     setSearchParam(search);
+    event.target.reset();
   };
+
+  let inputContainerClassName = reversed
+    ? styles.inputContainer + " " + styles.reversed
+    : styles.inputContainer;
 
   useEffect(() => {
     return setSearchParam("");
@@ -20,8 +29,16 @@ const Search = () => {
       {searchParam ? (
         <Navigate to={`/recetas?search=${searchParam}`} replace={true} />
       ) : null}
-      <form onSubmit={handleSubmit}>
-        <input name="search" placeholder="Buscar receta" />
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <span className={inputContainerClassName}>
+          <CiSearch />
+          <input
+            defaultValue={defaultSearch}
+            name="search"
+            placeholder="Search..."
+            className={styles.input}
+          />
+        </span>
       </form>
     </>
   );
