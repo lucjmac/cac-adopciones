a1b757f7e15b2c0f7ee477aa14897ad560c8b887
carolinaciampini <140980993+carolinaciampini@users.noreply.github.com>
2024-05-07 22:49:56 -0300

    Contacto ready

--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -1,43 +1,59 @@
-import React, { useState } from "react";
-import { useForm } from "react-hook-form";
-import { Link } from "react-router-dom";
+
 import styles from "./Contacto.module.css"
+import { useState, useEffect } from "react";
+import get from "../utils/conexionAPI"
 
 
 const Contacto = () => {
 
+    const [randomRecipe, setRandomRecipe] = useState(null);
+
+    useEffect(() => {
+        get("/random.php").then((data) => {
+            if (data && data.meals && data.meals.length > 0) {
+                setRandomRecipe(data.meals[0]);
+            }
+        });
+    }, []);
+
     return (
-        
-        <div className={styles.contacto_container}>
-            <h2>CONTACT US!</h2>
-            <div className={styles.linea}></div>
-                <section className={styles.contacto_section}>
-                    <div className={styles.contacto_form}>
-                        <form action="">
-                            <div className={styles.form_group}>
-                                <label htmlFor="inputEmail">Email adress:</label>
-                            <input type="email" name="correo" placeholder="Enter the email address where you want to receive your reply..." />
-                            </div>
-                            <div className={styles.form_group}>
-                                <label htmlFor="text">Subject:</label>
-                            <input type="text" name="asunto" placeholder="Enter a brief description of your message..." />
-                            </div>
-                            <div className={styles.form_group}>
-                                <label htmlFor="text">Message:</label>
-                            <textarea name="mensaje" rows="4" placeholder="Enter your message here..."></textarea>
-                            </div>
-                            <div className={styles.form_group}>
-                                <button type="submit" name="enviar">Enviar</button>
-                            </div>
-                        </form>
+        <div>
+            <div className={styles.contacto_container}>
+                    <section className={styles.contacto_section}>
+                        <div className={styles.contacto_form}>
+                            <h2>CONTACT US!</h2>
+                            <div className={ styles.linea}></div>
+                                <form action="">
+                                    <div className={styles.form_group}>
+                                        <label htmlFor="inputEmail">Email adress:</label>
+                                    <input type="email" name="correo" placeholder="Enter the email address where you want to receive your reply..." />
+                                    </div>
+                                    <div className={styles.form_group}>
+                                        <label htmlFor="text">Subject:</label>
+                                    <input type="text" name="asunto" placeholder="Enter a brief description of your message..." />
+                                    </div>
+                                    <div className={styles.form_group}>
+                                        <label htmlFor="text">Message:</label>
+                                    <textarea name="mensaje" rows="4" placeholder="Enter your message here..."></textarea>
+                                    </div>
+                                    <div className={styles.form_group}>
+                                        <button type="submit" name="enviar">Enviar</button>
+                                    </div>
+                                </form>
+                        </div>
+                     </section>
+                
+                 <div className={styles.image_recipe}>
+                    {randomRecipe && (
+                        <img
+                            src={randomRecipe.strMealThumb}
+                            alt={randomRecipe.strMeal}
+                            className={styles.random_recipe_image}
+                        />
+                    )}
                     </div>
-            </section>
-            
-            <div>
-                {/* Foto de alguna receta */}
+                </div>
             </div>
-            
-        </div>
     )
 }
 
