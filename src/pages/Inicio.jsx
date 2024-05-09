60f5e93578a4d4284c46a651eef146abc7e92d1d
Angela Delfino <angelairisguevara@gmail.com>
2024-05-09 05:40:47 -0300

    Agrega el llamado a la api e implementa el componente hero

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -1,8 +1,19 @@
+import { useEffect, useState } from "react";
+import Hero from "../Components/Hero/Hero";
 import SliderGrid from "../Components/Slider/SliderGrid";
+import get from "../utils/conexionAPI";
 
 const Inicio = () => {
+  const [recetas, setRecetas] = useState([]);
+
+  useEffect(() => {
+    get("/search.php?s=").then((data) => {
+      setRecetas(data.meals);
+    });
+  }, []);
   return (
     <>
+      <Hero recetas={recetas} />
       <SliderGrid />
     </>
   );
