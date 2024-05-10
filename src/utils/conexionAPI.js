db256482d6d66646739389c271e52c094fe22712
Angela Delfino <angelairisguevara@gmail.com>
2024-05-09 22:11:40 -0300

    Agrega el metodo getById para traer una receta especifica

--- src/utils/conexionAPI.js
+++ src/utils/conexionAPI.js
@@ -1,7 +1,13 @@
 const API = "https://www.themealdb.com/api/json/v1/1";
 
 const get = (path) => {
-    return fetch(API + path).then((results) => results.json());
+  return fetch(API + path).then((results) => results.json());
 };
 
-export default get
+export const getById = async (id) => {
+  return await fetch(API + `/lookup.php?i=${id}`).then((results) =>
+    results.json()
+  );
+};
+
+export default get;
