d55ee2e722fa734e7a9287ec6c3b88df45f6116a
Angela Delfino <angelairisguevara@gmail.com>
2024-05-09 22:05:03 -0300

    Implementa el componente Spinner y la prop de loading

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,8 +1,9 @@
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import styles from "./Hero.module.css";
+import Spinner from "../Spinner/Spinner";
 
-const Hero = ({ recetas = [] }) => {
+const Hero = ({ recetas = [], loading }) => {
   const [receta, setReceta] = useState();
 
   const navigate = useNavigate();
@@ -10,25 +11,30 @@ const Hero = ({ recetas = [] }) => {
   var day = new Date().getDay();
   const index = day % recetas.length;
   if (!receta && recetas.length > 0) setReceta(recetas[index]);
+
   return (
     <section className={styles.container}>
       <article
         className={styles.wrapper}
         onClick={() => navigate(`/receta/${receta?.idMeal}`)}
       >
-        <div className={styles.content}>
-          <div className={styles.imgContainer}>
-            <picture>
-              <img
-                src={receta?.strMealThumb}
-                alt={receta?.strMeal}
-                width={700}
-              />
-            </picture>
+        {loading ? (
+          <Spinner />
+        ) : (
+          <div className={styles.content}>
+            <div className={styles.imgContainer}>
+              <picture>
+                <img
+                  src={receta?.strMealThumb}
+                  alt={receta?.strMeal}
+                  width={700}
+                />
+              </picture>
+            </div>
+            <h1 className={styles.title}>Recipe of the day</h1>
+            <h2 className={styles.name}>{receta?.strMeal}</h2>
           </div>
-          <h1 className={styles.title}>Recipe of the day</h1>
-          <h2 className={styles.name}>{receta?.strMeal}</h2>
-        </div>
+        )}
       </article>
     </section>
   );
