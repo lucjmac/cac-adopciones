42f9e76db3af898114541704953ece7c64369556
Angela Delfino <angelairisguevara@gmail.com>
2024-05-14 10:21:31 -0300

    Implementa los nuevos menues dependiendo del ancho del navegador

--- src/Components/MainNav/MainNav.jsx
+++ src/Components/MainNav/MainNav.jsx
@@ -1,34 +1,13 @@
-import { NavLink, useLocation } from "react-router-dom";
-import MainNavItem from "./MainNavItem/MainNavItem";
-import Branding from "../Branding/Branding";
-import Search from "../Search/Search";
+import DesktopMenu from "./DesktopMenu/DesktopMenu";
+import MobileMenu from "./MobileMenu/MobileMenu";
+
 import styles from "./MainNav.module.css";
-import SurpriseMe from "../Global/SurpriseMe/SurpriseMe";
 
 const MainNav = () => {
-  const { pathname } = useLocation();
-  const isSearchPage = pathname === "/search";
-
   return (
     <nav className={styles.nav}>
-      <ul className={styles.container}>
-        <MainNavItem>
-          <NavLink to="/">
-            <Branding />
-          </NavLink>
-        </MainNavItem>
-        <MainNavItem>
-          {!isSearchPage ? <Search reversed={true} /> : null}
-        </MainNavItem>
-        <MainNavItem>
-          <SurpriseMe />
-        </MainNavItem>
-        <MainNavItem>
-          <NavLink className={styles.link} to="/recetas">
-            Recipes
-          </NavLink>
-        </MainNavItem>
-      </ul>
+      <DesktopMenu />
+      <MobileMenu />
     </nav>
   );
 };
